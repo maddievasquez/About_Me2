8319701184f218951ad5dc5c5286e8145dd58ccb
Timmyy <x001225270@gmail.com>
2022-03-03 22:06:25 +0000

    Buttons have been created on a linear layout

--- app/src/main/java/com/maddie/aboutme/EducationActivity.kt
+++ app/src/main/java/com/maddie/aboutme/EducationActivity.kt
@@ -1,11 +1,20 @@
 package com.maddie.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class EducationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_education)
+        val educationBtn = findViewById<Button>(R.id.educationBtn)
+        educationBtn.setOnClickListener {
+            val Intent = Intent(this, EducationActivity::class.java)
+            startActivity(Intent)
+        }
+
     }
-}
+
+    }
--- app/src/main/java/com/maddie/aboutme/MainActivity.kt
+++ app/src/main/java/com/maddie/aboutme/MainActivity.kt
@@ -2,6 +2,7 @@ package com.maddie.aboutme
 import android.content.Intent
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import android.os.Bundle as Bundle1
@@ -9,11 +10,15 @@ import android.os.Bundle as Bundle1
 
 //import androidx.appcompat.widget.Toolbar;
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle1?) {
+
+
         // Note that the Toolbar defined in the layout has the id "my_toolbar"
         val toolbar: Toolbar = findViewById<View>(R.id.toolbar_main) as Toolbar
+//        val toolbarTextView = findViewById<View>(android.R.id.toolbarTextView) as TextView
         setSupportActionBar(toolbar);
 
         super.onCreate(savedInstanceState)
